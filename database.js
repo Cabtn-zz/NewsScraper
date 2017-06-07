927e9643136c5bcdf3210bf44349ec031d200dd0
Abtin Ravanipour <Abtinr1185@gmail.com>
2017-06-06 18:17:42 -0700

    It works

--- database.js
+++ database.js
@@ -1,49 +1,105 @@
-/* MongoDB Zoo (18.2.3)
- * =================== */
-// STUDENTS: See near the bottom of this file for your TODO assignment.
-// Good luck!
-// Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+// var Note = require("./models/comments.js");
+var Article = require("./models/redditModel.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
 // Initialize Express
 var app = express();
-// Database configuration
-// Save the URL of our database as well as the name of our collection
-var databaseUrl = "mongo_scraper";
-var collections = ["reddit"];
-// Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
-// This makes sure that any errors are logged if mongodb runs into an issue
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/mongo_scraper");
+var db = mongoose.connection;
+
+// Show any mongoose errors
 db.on("error", function(error) {
-  console.log("Database Error:", error);
+  console.log("Mongoose Error: ", error);
 });
-// TODO: Make four routes that display results from your zoo collection
-// 0: Root: Displays a simple "Hello World" message (no mongo required)
-app.get("/", function(req, res) {
-  res.send("Hello world");
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
 });
-// 1: All: Show every animal in a json
-app.get("/all", function(req, res) {
-  // Query: In our database, go to the animals collection, then "find" everything
-  db.animals.find({}, function(err, data) {
-    // Log any errors if the server encounters one
-    if (err) {
-      return console.log(err);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.json(data);
-    }
+
+
+// Routes
+// ======
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("http://www.echojs.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
   });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
 });
-app.get("/name", (req, res) => {
-  db.animals.find({}).sort({name: -1}, (err, data) => {
-    res.json(data);
-  });
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+
+
+
 });
-// 2: Name: Sort results by name in ascending order, in a json
-// 3: Weight: Sort results by weight in descending order, in a json
-// Set the app to listen on port 3000
+
+// This will grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+
+
+});
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {
+
+});
+
+
+// Listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
 });
--- server.js
+++ server.js
@@ -1,216 +1,88 @@
 const express = require("express");
-const mongojs = require("mongojs");
 const app = express();
+
 const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 4000;
+
+const mongojs = require("mongojs");
+const PORT = process.env.PORT || 3000;
 const exphbs = require("express-handlebars");
 // Snatches HTML from URLs
 const request = require("request");
 // Scrapes our HTML
 const cheerio = require("cheerio");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const Reddit = require("./redditModel.js");
 
 //Setting up the mongo database
 const databaseUrl = "mongo_scraper";
 const collections = ["reddit"];
-const db = mongojs(databaseUrl, collections);
+// const db = mongojs(databaseUrl, collections);
 
-db.reddit.insert
+// db.reddit.insert
+
+mongoose.Promise = Promise;
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+mongoose.connect("mongodb://localhost/mongo_scraper");
+
+const db = mongoose.connection;
 
 
-// Making a request call for reddit's "webdev" board. The page's HTML is saved as the callback's third argument
-request("https://www.reddit.com/r/webdev", function(error, response, html) {
-  console.log("INITIALIZING SCRAPER");
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(html);
-  // An empty array to save the data that we'll scrape
-  // var result = [];
-  // With cheerio, find each p-tag with the "title" class
-  // (i: iterator. element: the current element)
-  console.log("GATHERING DATA");
-  $("p.title").each(function(i, element) {
-    // Save the text of the element (this) in a "title" variable
-    var title = $(this).text();
-    // In the currently selected element, look at its child elements (i.e., its a-tags),
-    // then save the values for any "href" attributes that the child elements may have
-    var link = $(element).children().attr("href");
-    // Save these results in an object that we'll push into the result array we defined earlier
-    console.log("PUSHING DATA");
-    db.reddit.insert({
-      title: title,
-      link: link
-    });
-  });
-    db.reddit.find({}, data => {
-      if(data) {
-        console.log(db.reddit.find({}))
-      }
-      else {
-        console.log("ASYNC")
-      }
-  });
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
 });
 
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
 
-//================================================================================
+//ROUTES
 
-/* Book Marker Warm Up (18.3.1)
- * backend
- * ==================== */
+app.get("/", (req,res) => {
+  // Making a request call for reddit's "webdev" board. The page's HTML is saved as the callback's third argument
+  request("https://www.reddit.com/r/webdev", function(error, response, html) {
+    console.log("INITIALIZING SCRAPER");
 
-// // Dependencies
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var logger = require("morgan");
-// var mongojs = require("mongojs");
-//
-// // Initialize Express
-// var app = express();
-//
-// // Configure our app for morgan and body parser
-// app.use(logger("dev"));
-// app.use(bodyParser.urlencoded({
-//   extended: false
-// }));
-//
-// // Static file support with public folder
-// app.use(express.static("public"));
-//
-// // Mongojs configuration
-// var databaseUrl = "week18day3";
-// var collections = ["books"];
-//
-// // Hook our mongojs config to the db var
-// var db = mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
-//
-//
-// // Routes
-// // ======
-//
-// // Post a book to the mongoose database
-// app.post("/submit", function(req, res) {
-//
-//   // Save the request body as an object called book
-//   var book = req.body;
-//
-//   // If we want the object to have a boolean value of false,
-//   // we have to do it here, because the ajax post will convert it
-//   // to a string instead of a boolean
-//   book.read = false;
-//
-//   // Save the book object as an entry into the books collection in mongo
-//   db.books.save(book, function(error, saved) {
-//     // Show any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the response to the client (for AJAX success function)
-//     else {
-//       res.send(saved);
-//     }
-//   });
-// });
-//
-//
-// // Find all books marked as read
-// app.get("/read", function(req, res) {
-//   // Go into the mongo collection, and find all docs where "read" is true
-//   db.books.find({ "read": true }, function(error, found) {
-//     // Show any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the books we found to the browser as a json
-//     else {
-//       res.json(found);
-//     }
-//   });
-// });
-//
-//
-// // Find all books marked as unread
-// app.get("/unread", function(req, res) {
-//   // Go into the mongo collection, and find all docs where "read" is false
-//   db.books.find({ "read": false }, function(error, found) {
-//     // Show any errors
-//     if (error) {
-//       console.log(error);
-//     }
-//     // Otherwise, send the books we found to the browser as a json
-//     else {
-//       res.json(found);
-//     }
-//   });
-// });
-//
-//
-// // Mark a book as having been read
-// app.get("/markread/:id", function(req, res) {
-//   // Remember: when searching by an id, the id needs to be passed in
-//   // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-//
-//   // Update a doc in the books collection with an ObjectId matching
-//   // the id parameter in the url
-//   db.books.update({
-//     "_id": mongojs.ObjectId(req.params.id)
-//   }, {
-//     // Set "read" to true for the book we specified
-//     $set: {
-//       "read": true
-//     }
-//   },
-//   // When that's done, run this function
-//   function(error, edited) {
-//     // show any errors
-//     if (error) {
-//       console.log(error);
-//       res.send(error);
-//     }
-//     // Otherwise, send the result of our update to the browser
-//     else {
-//       console.log(edited);
-//       res.send(edited);
-//     }
-//   });
-// });
-//
-//
-// // Mark a book as having been not read
-// app.get("/markunread/:id", function(req, res) {
-//   // Remember: when searching by an id, the id needs to be passed in
-//   // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-//
-//   // Update a doc in the books collection with an ObjectId matching
-//   // the id parameter in the url
-//   db.books.update({
-//     "_id": mongojs.ObjectId(req.params.id)
-//   }, {
-//     // Set "read" to false for the book we specified
-//     $set: {
-//       "read": false
-//     }
-//   },
-//   // When that's done, run this function
-//   function(error, edited) {
-//     // Show any errors
-//     if (error) {
-//       console.log(error);
-//       res.send(error);
-//     }
-//     // Otherwise, send the result of our update to the browser
-//     else {
-//       console.log(edited);
-//       res.send(edited);
-//     }
-//   });
-// });
-//
-//
-// // Listen on port 3000
-// app.listen(3000, function() {
-//   console.log("App running on port 3000!");
-// });
+    const results = [];
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
+    const $ = cheerio.load(html);
+    // With cheerio, find each p-tag with the "title" class
+    console.log("GATHERING DATA");
+    $("p.title").each(function(i, element) {
+      // Save the text of the element (this) in a "title" variable
+      const title = $(this).text();
+      // In the currently selected element, look at its child elements (i.e., its a-tags),
+      // then save the values for any "href" attributes that the child elements may have
+      const link = $(element).children().attr("href");
+      // Save these results in an object that we'll push into the result array we defined earlier
+      console.log("PUSHING DATA: " + title );
+
+      results.push({
+        title: title,
+        link: link,
+        comment: "SAY SOMETHING!"
+      })
+    });
+    const handlebars = {
+      result: results
+    }
+    const scrapedData = new Reddit(results);
+    scrapedData.save(results);
+    console.log("Sup mofo")
+    // res.send("SHIT WORKS")
+    res.json(results)
+  });
+})
+
+// Listen on port 3000
+app.listen(PORT, function() {
+  console.log("App running on port 3000!");
+});
